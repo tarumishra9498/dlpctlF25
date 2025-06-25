49affce648a658a6db44dc2f190c678fd11f7cd9
Connor <c.chua0007@gmail.com>
2025-06-25 11:03:40 -0500

    intial commit

---
+++ src/hough_params.py
@@ -0,0 +1,26 @@
+HOUGH_PARAMS = {
+
+    # non nanoparticle, .75 scale
+    # "med_blur" : 9,
+    # "p1" : 36,
+    # "p2" : 35,
+    # "minR" : 2,
+    # "maxR" : 40,
+    # "min_dist" : 46,
+
+    # nanoparticle, .75 scale
+    "med_blur" : 17,
+    "p1" : 72,
+    "p2" : 30,
+    "minR" : 2,
+    "maxR" : 40,
+    "min_dist" : 46,
+
+    # # nanoparticle, .5 scale
+    # "med_blur" : 11,
+    # "p1" : 40,
+    # "p2" : 26,
+    # "minR" : 2,
+    # "maxR" : 30,
+    # "min_dist" : 20
+}
